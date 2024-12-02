d4fd8a5e5d810f21f2cf7f443a85e9e55f39a609
Richard Casanova <richardcasanova@Richards-MacBook-Pro.local>
2024-12-01 21:10:31 -0500

    remove login

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Main_Components/Navbar";
 import AddHabit from "./components/Main_Components/AddHabbit";
 import Dashboard from "./components/Main_Components/Dashboard";
-import Login from "./components/Auth_Components/Login";
-import ProtectedRoute from "./components/Auth_Components/ProtectedRoute";
 import SignUp from "./components/Auth_Components/SignUp";
 import EditHabit from "./components/Main_Components/EditHabit";
 
@@ -14,34 +12,13 @@ const App: React.FC = () => {
     <div className="min-h-screen bg-gray-100">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        {/* Default route set to Dashboard */}
+        <Route path="/" element={<Dashboard />} />
 
-        <Route
-          path="/dashboard"
-          element={<Dashboard />}
-          // element={
-          //   <ProtectedRoute>
-          //     <Dashboard />
-          //   </ProtectedRoute>
-          // }
-        />
-        <Route
-          path="/add-habit"
-          element={
-            <ProtectedRoute>
-              <AddHabit />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/edit-habit/:id"
-          element={
-            <ProtectedRoute>
-              <EditHabit />
-            </ProtectedRoute>
-          }
-        />
+        {/* Other Routes */}
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/add-habit" element={<AddHabit />} />
+        <Route path="/edit-habit/:id" element={<EditHabit />} />
       </Routes>
     </div>
   );
